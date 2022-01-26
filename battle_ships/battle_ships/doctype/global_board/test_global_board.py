8baef7fcfca9154e23e3d792540e9710c10c5da5
kamaljohnson <kamaljohnson12345@gmail.com>
2022-01-26 20:31:39 +0530

    feat: add player id fetch from server (unique player)

---
+++ battle_ships/battle_ships/doctype/global_board/test_global_board.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Kamal Johnson and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestGlobalBoard(unittest.TestCase):
+	pass
--- battle_ships/www/battle/index.py
+++ battle_ships/www/battle/index.py
@@ -4,3 +4,9 @@ import frappe
 def upload_result(player_id, result):
     print(f"PLAYER ID: {player_id}")
     print(f"RESULT: {result}")
+
+@frappe.whitelist(allow_guest=True)
+def create_session_player():
+    new_player = frappe.new_doc("Player")
+    new_player.insert()
+    return new_player.name
